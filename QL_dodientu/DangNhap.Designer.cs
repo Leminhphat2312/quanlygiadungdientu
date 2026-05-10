a5d83b62a3d60459648b75bbba4eb808fdced4aa
Leminhphat2312 <dkzen1010@gmail.com>
2026-05-10 16:03:43 -0700

    Add files via upload

---
+++ QL_dodientu/DangNhap.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace QL_dodientu
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TXT_NAME = new System.Windows.Forms.TextBox();
+            this.TXT_PASS = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BTN_LOGIN = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TXT_NewName = new System.Windows.Forms.TextBox();
+            this.TXT_NewPass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(207, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng nhập";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label2.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 346);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 60);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label3.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 423);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 60);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật khẩu:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TXT_NAME
+            // 
+            this.TXT_NAME.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TXT_NAME.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_NAME.Location = new System.Drawing.Point(147, 346);
+            this.TXT_NAME.Name = "TXT_NAME";
+            this.TXT_NAME.Size = new System.Drawing.Size(400, 44);
+            this.TXT_NAME.TabIndex = 3;
+            // 
+            // TXT_PASS
+            // 
+            this.TXT_PASS.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TXT_PASS.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_PASS.Location = new System.Drawing.Point(223, 423);
+            this.TXT_PASS.Name = "TXT_PASS";
+            this.TXT_PASS.Size = new System.Drawing.Size(324, 44);
+            this.TXT_PASS.TabIndex = 4;
+            this.TXT_PASS.UseSystemPasswordChar = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.TXT_NewPass);
+            this.panel1.Controls.Add(this.TXT_NewName);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(625, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1853, 1595);
+            this.panel1.TabIndex = 5;
+            // 
+            // BTN_LOGIN
+            // 
+            this.BTN_LOGIN.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_LOGIN.Location = new System.Drawing.Point(179, 540);
+            this.BTN_LOGIN.Name = "BTN_LOGIN";
+            this.BTN_LOGIN.Size = new System.Drawing.Size(212, 131);
+            this.BTN_LOGIN.TabIndex = 6;
+            this.BTN_LOGIN.Text = "Đăng nhập";
+            this.BTN_LOGIN.UseVisualStyleBackColor = true;
+            this.BTN_LOGIN.Click += new System.EventHandler(this.BTN_LOGIN_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(255, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(308, 50);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tạo tài khoảng";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label5.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(73, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 60);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Tên:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label6.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(73, 423);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(157, 60);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Mật khẩu:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TXT_NewName
+            // 
+            this.TXT_NewName.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TXT_NewName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_NewName.Location = new System.Drawing.Point(163, 346);
+            this.TXT_NewName.Name = "TXT_NewName";
+            this.TXT_NewName.Size = new System.Drawing.Size(400, 44);
+            this.TXT_NewName.TabIndex = 7;
+            // 
+            // TXT_NewPass
+            // 
+            this.TXT_NewPass.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TXT_NewPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_NewPass.Location = new System.Drawing.Point(239, 423);
+            this.TXT_NewPass.Name = "TXT_NewPass";
+            this.TXT_NewPass.Size = new System.Drawing.Size(324, 44);
+            this.TXT_NewPass.TabIndex = 7;
+            this.TXT_NewPass.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(293, 540);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 131);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Tạo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1578, 844);
+            this.Controls.Add(this.BTN_LOGIN);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TXT_PASS);
+            this.Controls.Add(this.TXT_NAME);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.DangNhap_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TXT_NAME;
+        private System.Windows.Forms.TextBox TXT_PASS;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BTN_LOGIN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TXT_NewPass;
+        private System.Windows.Forms.TextBox TXT_NewName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ QL_dodientu/DangNhap.cs
@@ -0,0 +1,76 @@
+﻿using BLL_QLDDT;
+using DTO_QLDDT;
+using System;
+using System.Reflection;
+using System.Windows.Forms;
+using index;
+using DAL_QLDDT;
+namespace QL_dodientu
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_LOGIN_Click(object sender, EventArgs e)
+        {
+            TaikhoanDTO tk = new TaikhoanDTO();
+            tk.Username = TXT_NAME.Text;
+            tk.Password = TXT_PASS.Text;
+
+            int maNV;
+            string tenNV;
+
+            if (TaikhoanBLL.Dangnhap(tk, out maNV, out tenNV))
+            {
+                MessageBox.Show($"Cảm ơn nhân viên {tenNV} đã tiếp quản công việc");
+
+                SanPham frm = new SanPham();
+                frm.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu");
+            }
+        }
+
+        private void DangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = TXT_NewName.Text.Trim();
+            string password = TXT_NewPass.Text.Trim();
+            string tenNV = TXT_NewName.Text.Trim();
+
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+                return;
+            }
+
+            if (TaikhoanDAL.KiemTraTonTai(username))
+            {
+                MessageBox.Show("Username đã tồn tại!");
+                return;
+            }
+
+            bool kq = TaikhoanDAL.DangKy(username, password, tenNV);
+
+            if (kq)
+            {
+                MessageBox.Show("Đăng ký thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Đăng ký thất bại!");
+            }
+        }
+    }
+}
